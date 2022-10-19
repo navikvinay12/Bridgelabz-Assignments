ad16538b5f863c268b658e49d1bf113e6c64d48d
navikvinay12 <77015568+navikvinay12@users.noreply.github.com>
2022-10-19 15:30:26 +0530

    PS2-Q9 - read single digit no & display words

---
+++ casesingledigitnumber.sh
@@ -0,0 +1,37 @@
+echo enter any single digit number
+read N
+case $N in
+
+	0)
+        echo ZERO
+		;;
+	1)
+        echo ONE
+		;;
+	2)	
+        echo TWO
+		;;
+	3)	
+        echo THREE
+		;;
+	4)	
+		echo FOUR
+		;;
+	5)	
+        echo FIVE
+		;;
+	6)	
+        echo SIX
+		;;
+	7)	
+        echo SEVEN
+		;;
+	8)	
+        echo EIGHT
+		;;
+	9)	
+	echo NINE
+		;;
+	10)
+	echo please enter only single digit number i.e 0 to 9 .
+esac
